d8ca6031befc782180ea892c6d6cb643369ebbae
Josh <joshGdovindesigns.com>
2022-08-28 21:00:24 -0400

    uodate

--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -1,1 +1,26 @@
-let burgerBtn=document.querySelector("#burger-btn"),mainMenu=document.querySelector("#main-menu"),canSeeMenu=!1;burgerBtn.addEventListener("click",(()=>{console.log(canSeeMenu),canSeeMenu?(mainMenu.classList.remove("show-menu"),canSeeMenu=!1):(mainMenu.classList.add("show-menu"),canSeeMenu=!0)}));let closeBtn=document.querySelector("#close-btn");closeBtn.addEventListener("click",(()=>{mainMenu.classList.remove("show-menu"),canSeeMenu=!1}));
+
+
+let burgerBtn = document.querySelector("#burger-btn");
+let mainMenu = document.querySelector("#main-menu");
+
+let canSeeMenu = false;
+
+burgerBtn.addEventListener("click", () => {
+    console.log(canSeeMenu);
+
+    if(!canSeeMenu){
+        mainMenu.classList.add("show-menu");
+        canSeeMenu = true;
+    }else{
+        mainMenu.classList.remove("show-menu");
+        canSeeMenu = false;
+    }
+})
+
+
+let closeBtn = document.querySelector("#close-btn");
+
+closeBtn.addEventListener("click", () => {
+        mainMenu.classList.remove("show-menu");
+        canSeeMenu = false;
+})
